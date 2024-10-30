3ce3931291e357861f2d255aba83a51c71ff83f4
HarshilTomar20 <harshiltomar20@gmail.com>
2024-10-30 19:20:46 +0530

    chore: theme init, header init

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,33 @@
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Inter } from 'next/font/google';
+import Header from "@/components/header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"]})
 
 export const metadata = {
   title: "Scrumify",
-  description: "Next JS, Shadcn UI, Tailwind, Clerk, NeonDB based Jira sort of Platform",
+  description:
+    "Next JS, Shadcn UI, Tailwind, Clerk, NeonDB based Jira sort of Platform",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className}`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          <Header/>
+          <main className="min-h-screen">{children}</main>
+          <footer className="container mx-auto px-4 text-center text-gray-600">
+            <p>Made with 💗 by Harshil</p>
+          </footer>          
+        </ThemeProvider>
       </body>
     </html>
   );
